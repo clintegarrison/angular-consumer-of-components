505e7b79f4c28a884e394f4ab6a0923a002c1db8
clintegarrison <clintegarrison@gmail.com>
2022-08-27 10:03:32 -0400

    heroku troubleshooting

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/angular-consumer-of-components'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/angular-consumer-of-components/index.html'));});
+    res.sendFile(path.join(__dirname+'/dist/angular-consumer-of-components/index.html'));
+});
 app.listen(process.env.PORT || 8080);
