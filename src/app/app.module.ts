b4b2cadba9097fcd83fdb0f1c776f7ae99937367
clintegarrison <clintegarrison@gmail.com>
2022-08-27 09:34:33 -0400

    consume component and serve for heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PageModule } from 'angular-components-depot-library'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
